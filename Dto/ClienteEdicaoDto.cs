526e8f47b71a1d7993099acafa7f5ddbac2303ea
Lucas Alberto <lucasalberto4321@gmail.com>
2025-09-24 14:32:41 -0300

    feat: model-context-dto

---
+++ Dto/ClienteEdicaoDto.cs
@@ -0,0 +1,7 @@
+public class ClienteEdicaoDto
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Sobrenome { get; set; }
+    public string Endereco { get; set; } 
+}
---
+++ Models/ClienteModel.cs
@@ -0,0 +1,9 @@
+public class ClienteModel
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Sobrenome { get; set; }
+    public string Cpf { get; set; }
+    public string Email { get; set; }
+    public string Endereco { get; set; }
+}
